adb7022cb8838122b59d5eb9eed6f4304144a797
Jagadeesh <jagadeesh.r@emsibg.com>
2024-10-21 12:46:56 +0530

    checked security issues

--- MyBlog/my-blog/pages/chart.tsx
+++ MyBlog/my-blog/pages/chart.tsx
@@ -1,6 +1,8 @@
 // pages/chart.tsx
 
 const FlourishChartPage = () => {
+    const chartID = process.env.NEXT_PUBLIC_FLOURISH_CHART_KEY;
+
     return (
       <div className="max-w-7xl mx-auto py-12 px-6" >
         <h2 className="text-center text-4xl font-bold mb-10 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
@@ -8,7 +10,7 @@ const FlourishChartPage = () => {
         </h2>
         <div className="w-full">
           <iframe
-            src="https://flo.uri.sh/visualisation/19792598/embed"  // your Flourish chart key
+            src={`https://flo.uri.sh/visualisation/${chartID}/embed`}   // your Flourish chart key
             frameBorder="0"
             scrolling="no"
             className="w-[1000px] h-[500px] max-w-full block"
