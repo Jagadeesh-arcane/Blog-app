49f17042a2cd0191374be13e13d98cc32a1f969a
Jagadeesh <jagadeesh.r@emsibg.com>
2024-10-24 15:46:08 +0530

    Added custom styles to hubspot form

--- MyBlog/my-blog/pages/contact.tsx
+++ MyBlog/my-blog/pages/contact.tsx
@@ -25,18 +25,17 @@ const HubSpotForm = () => {
   const generateForm = useCallback(() => {
     const portalId = process.env.NEXT_PUBLIC_HUBSPOT_FORM_PORTAL_ID;
     const formId = process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID;
-    const targetId = 'hubspotForm';               // ID for the form container
+    const targetId = 'hubspotForm';
 
     if (typeof window !== 'undefined' && window.hbspt) {
       const formEl = document.getElementById(targetId);
 
       if (formEl && formEl.innerHTML === '') {
-        // Create the HubSpot form as raw HTML and disable default HubSpot CSS
         window.hbspt.forms.create({
           portalId: portalId!,
           formId: formId!,
-          target: `#${targetId}`,                 // Target ID where the form will be rendered
-          css: true, // Enable HubSpot's default CSS
+          target: `#${targetId}`,
+          css: false,
         });
       }
     }
@@ -45,38 +44,114 @@ const HubSpotForm = () => {
   useEffect(() => {
     if (!isFormScriptLoaded) {
       const script = document.createElement('script');
-      script.src = '//js.hsforms.net/forms/v2.js'; 
+      script.src = '//js.hsforms.net/forms/v2.js';
       script.async = true;
       document.body.appendChild(script);
 
       script.onload = () => {
         setIsFormScriptLoaded(true);
-        generateForm(); 
+        generateForm();
       };
     }
   }, [isFormScriptLoaded, generateForm]);
 
   return (
-    <div className="bg-transparent min-h-screen text-white flex flex-col items-center justify-center">
-      <section className="py-12 px-6 text-center">
-        <h2 className="text-4xl font-bold mb-10 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
+    <div className="bg-transparent min-h-screen text-white">
+      <section className="py-12 px-6">
+        <h2 className="text-center text-4xl font-bold mb-10 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent mr-2">
           Contact Us
         </h2>
-        <div className="group bg-gray-900 p-6 rounded-lg shadow-lg hover:shadow-2xl transition transform hover:-translate-y-2 mb-6">
-          <p className="text-gray-400 group-hover:text-white transition">
-            &quot;Please fill out the form to contact us, We will contact you shortly to discuss uploading your blog content to our site&quot;
-          </p>
+
+        <div className="grid grid-cols-1 gap-20">
+          <div className="group bg-gray-900 p-6 rounded-lg shadow-lg hover:shadow-2xl transition transform hover:-translate-y-2">
+            <p className="text-gray-400 text-center group-hover:text-white transition">
+              &quot;Please fill out the form to contact us, We will contact you shortly to add your blog content to our site&quot;
+            </p>
+          </div>
         </div>
       </section>
 
-      {/* HubSpot form container */}
-      <div id="hubspotForm" className="w-full max-w-md mx-auto"></div>
+      <div id="hubspotForm" className="my-8 ml-60 mr-60"></div>
 
       <div className="text-center mt-8">
-        <Link href="/" className="text-yellow-300 font-bold hover:text-red-500 transition duration-200">
+        <Link href="/" className="text-blue-700 font-bold hover:text-red-500 transition duration-200">
           Go Back to Home
         </Link>
       </div>
+
+      {/* CSS Styles directly in the component */}
+      <style type="text/css">{`
+        .hs-form input, 
+        .hbspt-form .hs-form textarea, 
+        .hbspt-form .hs-form select { 
+          background-color: #9E9E9E !important; /* Ensure gray background */
+          border: solid 2px #FF69B4;
+          font-size: 16px;
+          color: #003366;
+          -moz-border-radius: 50px;
+          -webkit-border-radius: 50px;
+          border-radius: 50px;
+          padding: 5px 10px;
+          margin: 5px 0px;
+        }
+        .hs-form input::placeholder, 
+        .hbspt-form .hs-form textarea::placeholder {
+          font-size: 15px;
+          color: #000000;
+        }
+        .hs-form .input label {
+          font-size: 16px;
+          color: #000000;
+          font-weight: normal;
+          padding-top: 0px;
+          padding-bottom: 0px;
+          display: block;
+        }
+        .hs-form .actions .hs-button.primary.large {
+          background-color: #D5006D !important;
+          border: solid 3px #FF69B4;
+          font-size: 16px;
+          color: #FFFFFF;
+          font-weight: bold;
+          padding-top: 10px;
+          padding-bottom: 10px;
+          padding-right: 10px;
+          padding-left: 10px;
+          -moz-border-radius: 11px;
+          -webkit-border-radius: 11px;
+          border-radius: 11px;
+        }
+        .hs-form .actions {
+          margin-top: 2px;
+          margin-bottom: 0px;
+          text-align: center;
+        }
+        ul.hs-error-msgs {
+          padding: 0px 0px 10px 0px;
+          list-style-type: none;   
+          font-size: 14px;
+          color: #FF0000;
+          font-weight: normal;
+          text-align: left;
+        }
+        .submitted-message p {
+          font-size: 16px;
+          color: #80FF00;
+          font-weight: normal;
+          text-align: left;
+        }
+        .hs-form textarea {
+          height: 68px;
+          resize: none;
+        }
+        .hbspt-form .hs-form .input label {
+          float: none;
+          text-align: left;
+        }
+        .submitted-message {
+          min-height: 50px;
+        }
+      `}</style>
     </div>
   );
 };
